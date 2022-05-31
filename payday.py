b32f4bb46365bb7f21f41a10d4e032daff9b5445
pjpuzzler <pjpuzzler@gmail.com>
2022-05-31 09:49:39 -0400

    c

--- payday.py
+++ payday.py
@@ -1,3 +1,4 @@
+from calendar import week
 import curses
 from datetime import date, datetime, time, timedelta
 from numpy import busday_count
@@ -13,6 +14,7 @@ DAILY_HOURS_WORKED = 7
 BREAK_TIME = time(12)  # 12:00 PM
 BREAK_DURATION = timedelta(hours=1)
 TAX_PERCENTAGE = 0.2233  # 22.33%
+WEEKMASK = [1, 1, 1, 1, 1, 0, 0]  # Mon-Fri
 HOLIDAYS = [date(2022, 5, 30)]
 TIME_OFF = timedelta(hours=0)
 PAY_INTERVAL = timedelta(weeks=2)  # Biweekly
@@ -34,7 +36,7 @@ while True:
     day_end = datetime.combine(now.date(), END_TIME)
 
     time_worked_prev = timedelta(hours=int(busday_count(
-        START_DATE.date(), now.date(), holidays=HOLIDAYS) * DAILY_HOURS_WORKED)) - TIME_OFF
+        START_DATE.date(), now.date(), holidays=HOLIDAYS, weekmask=WEEKMASK) * DAILY_HOURS_WORKED)) - TIME_OFF
 
     # completed a full day today
     if now >= day_end:
@@ -64,11 +66,15 @@ while True:
         (1 - TAX_PERCENTAGE)
 
     prev_payday = next_payday - PAY_INTERVAL
-    time_worked_before_payday = (timedelta(hours=int(busday_count(START_DATE.date(), (prev_payday + timedelta(
-        days=1)).date(), holidays=HOLIDAYS) * DAILY_HOURS_WORKED)) - TIME_OFF) if prev_payday > START_DATE else timedelta()
+    time_worked_before_payday = (timedelta(hours=int(busday_count(START_DATE.date(), prev_payday.date() + timedelta(
+        days=1), holidays=HOLIDAYS, weekmask=WEEKMASK) * DAILY_HOURS_WORKED)) - TIME_OFF) if prev_payday > START_DATE else timedelta()
     time_worked_payday = time_worked - time_worked_before_payday
     money_earned_payday = time_worked_payday.total_seconds() * HOURLY_WAGE / \
         3600 * (1 - TAX_PERCENTAGE)
+    total_payday_time = timedelta(hours=int(busday_count(
+        prev_payday.date() + timedelta(days=1), next_payday.date() + timedelta(days=1), holidays=HOLIDAYS, weekmask=WEEKMASK) * DAILY_HOURS_WORKED))
+    total_payday_money = total_payday_time.total_seconds() * HOURLY_WAGE / 3600 * \
+        (1 - TAX_PERCENTAGE)
 
     stdscr.addstr(
         0, 0, f'Gross Earnings: ${money_earned:,.2f} (${HOURLY_WAGE / 3600:,.4f}/s)')
@@ -80,7 +86,7 @@ while True:
         2, 0, f'Net Earnings:   ${(money_earned - taxes_paid):,.2f} (${HOURLY_WAGE / 3600 * (1 - TAX_PERCENTAGE):,.4f}/s)')
 
     stdscr.addstr(
-        4, 0, f'Payday Earnings: ${money_earned_payday:,.2f}/${TOTAL_PAYDAY_EARNINGS:,.2f}')
+        4, 0, f'Payday Earnings: ${money_earned_payday:,.2f}/${total_payday_money:,.2f}')
     stdscr.addstr(
         4, 40, f'Time Until Payday ({payday_num}): {naturaldelta(time_until_payday)}')
 
